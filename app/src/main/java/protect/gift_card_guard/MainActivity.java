7284f70d546f94722f1241fd0024484c7d87ae56
Branden Archer <Branden Archer b.m.archer4@gmail.com>
2016-01-24 12:06:42 -0500

    initial commit of empty main activity

---
+++ app/src/main/java/protect/gift_card_guard/MainActivity.java
@@ -0,0 +1,40 @@
+package protect.gift_card_guard;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.add_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == R.id.action_add)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
