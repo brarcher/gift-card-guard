9d73c1a77c793f04732f5e408c60b9084ea59b89
Branden Archer <Branden Archer b.m.archer4@gmail.com>
2016-01-24 20:25:19 -0500

    Display gift cards in main activity

---
+++ app/src/main/java/protect/gift_card_guard/GiftCardCursorAdapter.java
@@ -0,0 +1,48 @@
+package protect.gift_card_guard;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+class GiftCardCursorAdapter extends CursorAdapter
+{
+    public GiftCardCursorAdapter(Context context, Cursor cursor)
+    {
+        super(context, cursor, 0);
+    }
+
+    // The newView method is used to inflate a new view and return it,
+    // you don't bind any data to the view at this point.
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent)
+    {
+        return LayoutInflater.from(context).inflate(R.layout.giftcard_layout, parent, false);
+    }
+
+    // The bindView method is used to bind all data to a given view
+    // such as setting the text on a TextView.
+    @Override
+    public void bindView(View view, Context context, Cursor cursor)
+    {
+        // Find fields to populate in inflated template
+        TextView storeField = (TextView) view.findViewById(R.id.store);
+        TextView valueField = (TextView) view.findViewById(R.id.value);
+        TextView cardIdField = (TextView) view.findViewById(R.id.cardId);
+
+        // Extract properties from cursor
+        GiftCard giftCard = GiftCard.toGiftCard(cursor);
+
+        // Populate fields with extracted properties
+        storeField.setText(giftCard.store);
+        valueField.setText(giftCard.value);
+
+        String cardIdFormat = view.getResources().getString(R.string.cardIdFormat);
+        String cardIdLabel = view.getResources().getString(R.string.cardId);
+        String cardIdText = String.format(cardIdFormat, cardIdLabel, giftCard.cardId);
+        cardIdField.setText(cardIdText);
+    }
+}
--- app/src/main/java/protect/gift_card_guard/MainActivity.java
+++ app/src/main/java/protect/gift_card_guard/MainActivity.java
@@ -1,15 +1,19 @@
 package protect.gift_card_guard;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -17,6 +21,56 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.main_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        updateGiftCardList();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        updateGiftCardList();
+    }
+
+    private void updateGiftCardList()
+    {
+        final ListView cardList = (ListView) findViewById(R.id.list);
+        final TextView helpText = (TextView) findViewById(R.id.helpText);
+        final DBHelper db = new DBHelper(this);
+
+        if(db.getGiftCardCount() > 0)
+        {
+            cardList.setVisibility(View.VISIBLE);
+            helpText.setVisibility(View.GONE);
+        }
+        else
+        {
+            cardList.setVisibility(View.GONE);
+            helpText.setVisibility(View.VISIBLE);
+        }
+
+        Cursor cardCursor = db.getGiftCardCursor();
+
+        final GiftCardCursorAdapter adapter = new GiftCardCursorAdapter(this, cardCursor);
+        cardList.setAdapter(adapter);
+
+        cardList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                Cursor selected = (Cursor) parent.getItemAtPosition(position);
+                GiftCard giftCard = GiftCard.toGiftCard(selected);
+
+                Intent i = new Intent(view.getContext(), GiftCardViewActivity.class);
+                final Bundle b = new Bundle();
+                b.putInt("id", giftCard.id);
+                b.putBoolean("update", true);
+                i.putExtras(b);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
